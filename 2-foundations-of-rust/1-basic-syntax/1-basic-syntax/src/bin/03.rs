ae36bb1a81b88ad02975ea5643947c1d2c8f0d2a
Adrian Vasiloaica <164257098+Adr20ian@users.noreply.github.com>
2024-10-24 16:57:06 +0000

    lab

--- 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
+++ 2-foundations-of-rust/1-basic-syntax/1-basic-syntax/src/bin/03.rs
@@ -2,6 +2,16 @@ fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
     // TODO
+    let mut smallest = input[0];
+    let mut largest = input[0];
+    for &num in &input {
+        if num < smallest
+        smallest = num;
 
-    println!("{} is largest and {} is smallest");
+    }
+    if num > largest {
+        largest = num;
+    }
+
+    println!("{} is largest and {} is smallest",largest,smallest);
 }
